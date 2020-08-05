5bea09c3b268c7df71ef9e4ee8ecffe986563baf
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2020-08-05 23:46:50 +0200

    Parsing is working

--- parsing.py
+++ parsing.py
@@ -1,24 +1,44 @@
 import os
 from html.parser import HTMLParser
+import csv
+
+
+# define data
+basepath = "htmls/"
+dir = os.listdir(basepath)
+length = len(dir)
+# data = numpy.asarray([[0, 0, 0]])
+data = []
+count = 0
 
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
+        if (tag == "div") & (len(attrs) == 7):
+            if attrs[4][0] == "data-initial-image-href":
+                data[count][0] = count
+                data[count][1] = attrs[5][1]
+                data[count][2] = (
+                    "https://ids.lib.harvard.edu/ids/view/"
+                    + attrs[5][1]
+                    + "?height=500&width=500"
+                )
+        if (tag == "div") 
 
 
 parser = MyHTMLParser()
 
-# List all files in a directory using os.listdir
-basepath = "urls/"
-for entry in os.listdir(basepath):
+for entry in dir:
+    if count == 10:
+        # print(html)
+        break
+    data.append([0, 0, 0])
     path = os.path.join(basepath, entry)
-    file = open(path,'r')
-    # if os.path.isfile():
-    parser.feed(file)
+    file = open(path, "r")
+    html = file.read()
+    parser.feed(html)
+    count += 1
+
+with open("data.csv", "w+") as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=",")
+    csvWriter.writerows(data)
