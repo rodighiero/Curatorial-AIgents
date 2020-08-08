c5297a8371abcbf5602cc4a0b171c00d63651cdb
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2020-08-08 21:06:59 +0200

    Parsing

--- parsing.py
+++ parsing.py
@@ -1,41 +1,67 @@
 import os
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 import csv
 
-
-# define data
+# Variables
 basepath = "htmls/"
 dir = os.listdir(basepath)
 length = len(dir)
-# data = numpy.asarray([[0, 0, 0]])
 data = []
 count = 0
 
-
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        if (tag == "div") & (len(attrs) == 7):
-            if attrs[4][0] == "data-initial-image-href":
-                data[count][0] = count
-                data[count][1] = attrs[5][1]
-                data[count][2] = (
-                    "https://ids.lib.harvard.edu/ids/view/"
-                    + attrs[5][1]
-                    + "?height=500&width=500"
-                )
-        # if (tag == "div") 
-
-
-parser = MyHTMLParser()
-
-for entry in dir:
-    data.append([0, 0, 0])
-    path = os.path.join(basepath, entry)
-    file = open(path, "r")
-    html = file.read()
-    parser.feed(html)
-    count += 1
-
 with open("data.csv", "w+") as my_csv:
     csvWriter = csv.writer(my_csv, delimiter=",")
-    csvWriter.writerows(data)
+    for entry in dir:
+
+        count += 1
+        print(count)
+
+        # Limit
+        # if count == 2:
+        #     break
+
+        
+
+        # Settings
+        path = os.path.join(basepath, entry)
+        file = open(path, "r")
+        html = file.read()
+        soup = BeautifulSoup(html, "html.parser")
+
+        # object
+        meta = soup.find("meta", property="og:url")
+        if meta == None:
+            continue
+        url = meta["content"]
+        object = url[len(url.rstrip("0123456789")) :]
+
+        # IIIF
+        id = soup.find_all("div", "osd")
+        if len(id) == 0:
+            continue
+        else:
+            id = id[0]['id'].split("_", 1)[1]
+
+        # URL
+        # div = soup.find_all("div", "osd")
+        # if len(div) == 0:
+        #     url = ""
+        # else:
+        #     url = div[0]["data-initial-image-href"].split("?", 1)[0] + "?height=500&width=500" + '&apikey=cf26e390-3f74-11ea-bcd7-11dad6a174b9'
+        url = 'https://ids.lib.harvard.edu/ids/view/' + id + '?width=500&height=500'
+
+        # Title
+        title = soup.find_all("span", "popup__artwork-info-title")[0].contents[0]
+
+        # Dimension
+        dimension = soup.find_all("dt", string="Dimensions")
+        if len(dimension) == 0:
+            dimension = ""
+        else:
+            dimension = dimension[0].find_next().contents[0]
+        
+        # Writing
+        row = [object, id, url, title, dimension]
+
+        
+        csvWriter.writerow(row)
